0c9bfeb370c9a6d816c6a7daaa5e2d46aa664ad1
MeetaHaldar <meetahaldar1001@gmail.com>
2025-03-31 16:45:38 +0530

    Added path for each page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 import './App.css'
 import CompanyRegistration from './Components/CompanyRegistration'
 import EmployeeRegister from './Components/EmployeeRegister'
@@ -8,9 +9,16 @@ import Login from './Components/Login'
 function App() {
 
   return (
-    // <CompanyRegistration/>
-    // <EmployeeRegister/>
-    <Login/>
+    <>
+     <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} /> 
+        <Route path="/companyregister" element={<CompanyRegistration />} /> 
+        <Route path="/employeeregister" element={<EmployeeRegister />} /> 
+        <Route path="*" element={<h1>404 - Page Not Found</h1>} />  
+      </Routes>
+    </Router>
+    </>
   )
 }
 
