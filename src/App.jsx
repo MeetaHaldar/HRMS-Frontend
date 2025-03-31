340074e4c30efefb83fc0d9be88f9775bfa6d44e
MeetaHaldar <meetahaldar1001@gmail.com>
2025-03-31 16:05:07 +0530

    Registation company completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+import './App.css'
+import CompanyRegistration from './Components/CompanyRegistration'
+
+function App() {
+
+  return (
+    <CompanyRegistration/>
+  )
+}
+
+export default App
---
+++ src/Components/CompanyRegistration.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+const CompanyRegistration = () => {
+  return (
+    <div className="flex flex-col md:flex-row items-center justify-center min-h-screen bg-white p-6">
+      <div className="hidden md:flex w-1/2 justify-center items-center">
+        <img
+          src="src\assets\Login_image.png"
+          alt="Illustration"
+          className="max-w-full h-auto"
+        />
+      </div>
+      
+      <div className="w-full md:w-1/2 bg-white p-6  border-gray-200">
+        <h2 className="text-2xl font-bold text-gray-800 mb-4 text-center">
+          Registration - <span className="font-semibold">Company</span>
+        </h2>
+        <form className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          {[
+            "Company Name*",
+            "Sub-Domain*",
+            "Contact Number*",
+            "Email Address*",
+            "Company Address*",
+            "City*",
+            "Country Code*",
+            "Company's Website",
+            "Max. Number of Employees*",
+            "Payment",
+            "Admin Name*",
+          ].map((placeholder, index) => (
+            <input
+              key={index}
+              type={placeholder.includes("Email") ? "email" : "text"}
+              placeholder={placeholder.replace("*", "")}
+              className="w-75 border-b border-gray-300 p-2 placeholder-gray-400 focus:outline-none focus:border-black"
+              required={placeholder.includes("*")}
+            />
+          ))}
+      
+          <input type="password" placeholder="Password" className="w-full border-b border-gray-400 p-2 placeholder-gray-400 focus:outline-none focus:border-black" required />
+        </form>
+        <div className="flex items-center justify-center mt-4">
+          <input type="checkbox" className="mr-2" required />
+          <p className="text-sm text-gray-600">By continuing, I agree to the <span className="font-semibold">Terms & Conditions</span> and <span className="font-semibold">Privacy Policy</span>.</p>
+        </div>
+
+<button className="w-72 mt-4 bg-gray-500 text-white text-lg font-semibold py-2 px-6 rounded-full hover:bg-yellow-500 transition block mx-auto">
+  Register
+</button>
+
+
+
+
+
+
+      </div>
+    </div>
+  );
+};
+
+export default CompanyRegistration;
